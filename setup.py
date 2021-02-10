ad0e7f65c4bb64a761decbf8cb02c5cf8bc3a8a8
Shao-Chieh Lien <liens@purdue.edu>
2021-02-10 11:07:55 -0500

    Add set.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xworker',
+    version='1.0',
+    author='Shao-chieh Lien',
+    author_email='leins@purdue.edu',
+    packages=find_packages(),
+)
+
