e3e7353b1c746bb320eddc14ca80804f86e4a1b8
Shao-chieh, Lien <liens@purdue.edu>
2021-03-08 22:47:21 -0500

    Update car_speed_detection_test.py

--- car_speed/car_speed_detection_test.py
+++ car_speed/car_speed_detection_test.py
@@ -220,7 +220,7 @@ def test_speed_detection():
         test_list[i] = float(test_list[i])
     # check MSE error, if > 1.5, fail, suppose to be 1.012948989868164
     mse = mean_squared_error(ans_list, test_list)
-    print(mse)
+    print("mse", mse)
     assert mse < 3
 
     print("test_speed_detection: PASS")
