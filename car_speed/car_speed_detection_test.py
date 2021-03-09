b9bdf2ac63f284a63753d0a8d66d0e464fdb4b1d
Shao-chieh, Lien <liens@purdue.edu>
2021-03-09 00:10:58 -0500

    Update car_speed_detection_test.py

--- car_speed/car_speed_detection_test.py
+++ car_speed/car_speed_detection_test.py
@@ -233,7 +233,7 @@ def test_speed_detection():
         sum_for_mse += (temp * temp)
     
     # mse = mean_squared_error(ans_list, test_list)
-    assert int(mse) < 3
+    assert int(sum_for_mse) < 3
 
     print("test_speed_detection: PASS")
     os.remove('test_case/test_speed_detection/test.txt')
