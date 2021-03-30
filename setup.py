2bb2dab36ed4a72448cf700c8c89c3ffe92270a4
Shao-Chieh Lien <liens@purdue.edu>
2021-03-30 18:12:45 -0400

    Modified the package name on setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='xworker',
+    name='car_speed_detection',
     version='0.7.0',
     author='Shao-chieh Lien',
     author_email='shaochiehlien@gmail.com',
