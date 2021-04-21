491f66af975341d04d5467d87c164b8a61f9f01a
Shao-Chieh Lien <liens@purdue.edu>
2021-04-21 12:28:02 -0400

    Reorganized

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ import os
 
 VERSION = '0.7.5'
 DESCRIPTION = 'Speed detection library for automobile'
-LONG_DESCRIPTION = 'A package that could serve as a speedometer for autonomous car using dashboard camera'
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
+
+with open("README.md") as f:
+    long_description = f.read()
 
 # Setting up
 setup(
@@ -17,16 +19,18 @@ setup(
     author_email='shaochiehlien@gmail.com',
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
     packages=find_packages(),
+    # Referred to: https://setuptools.readthedocs.io/en/latest/userguide/datafiles.html
+    package_data = {'car_speed': ['*/**/*', '*/**/**/*', '*/**/**/**/*', '*/**/**/**/**/*']},
     license='MIT',
     install_requires=required,
     keywords=['python', 'car speed detection', 'software-based speedometer', 'dashboard camera', 'optical flow', 'machine learning', 'keras'],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
+        "Operating System :: Linux",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
     ]
